122f985fac2f9b4509322489cdabc43bf5d46f98
an-victor <an.victor@gmail.com>
2025-05-30 17:19:33 -0300

    Corrige conflito de variáveis Sanity

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -5,7 +5,7 @@ import { createClient } from "next-sanity";
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "98pdr6kx";
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-05-30"; // Use a recent date
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_TOKEN || "skuZlhjQitawRtPSS0kSoGCYabb4rKFhcKV8ejubwqc864KZ34lTn4EEFliMAIo1zO0AewQPvuxR2PlX9JuBVQtHvpTHRWzTx5obauDAi7aV5Ml7pXCah1bTc7dLR1BKLeUVbpmSKmqIFGRfV4i9lY6reRXf70ivnqxIu57kXxRySDmn8IpX";
+export const token = process.env.SANITY_API_TOKEN; // "skuZlhjQitawRtPSS0kSoGCYabb4rKFhcKV8ejubwqc864KZ34lTn4EEFliMAIo1zO0AewQPvuxR2PlX9JuBVQtHvpTHRWzTx5obauDAi7aV5Ml7pXCah1bTc7dLR1BKLeUVbpmSKmqIFGRfV4i9lY6reRXf70ivnqxIu57kXxRySDmn8IpX";
 
 export const client = createClient({
   projectId,
